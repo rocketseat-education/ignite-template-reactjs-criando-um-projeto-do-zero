0266317000bf0c6aa0f663b0c50887d07c4ea923
NicholasWM <nicholaswesleymazzei@gmail.com>
2021-10-01 12:20:48 -0300

    add: Style of HomePage

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,7 +1,19 @@
-import Document from 'next/document';
+import Document, { Html, Head, Main, NextScript } from 'next/document'
 
 export default class MyDocument extends Document {
-  // render() {
-  //   //TODO
-  // }
+  render() {
+    return (
+      <Html>
+        <Head>
+            <link rel="preconnect" href="https://fonts.googleapis.com"/>
+            <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap" rel="stylesheet"/>
+            <link rel="shortcut icon" href="./favicon.png" type="image/png" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import { GetStaticProps } from 'next';
+import Head from 'next/head'
 
 import { getPrismicClient } from '../services/prismic';
 
@@ -24,9 +25,44 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home(props: HomeProps) {
+  return (
+    <>
+      <Head>
+        <title>Home | SpaceTraveling 42</title>
+      </Head>
+      <body>
+        <main className={styles.container}>
+          <img className={styles.logo} src="/images/logo.svg" alt="Space Traveling Blog" />
+          <article className={styles.post}>
+            <a href="">
+              <h1>Como utilizar Hooks</h1>
+              <p>Pensando em sincronização em vez de ciclos de vida.</p>
+              <div className={styles.postInfos}>
+                <div className={styles.updatedAt}>
+                  <img src="/images/calendar.svg" alt="" />
+                  <time>{new Date().toLocaleDateString('pt-BR', {
+                    day:'2-digit',
+                    month:'short',
+                    year:'numeric'
+                  })}</time>
+                </div>
+                <div className={styles.author}>
+                  <img src="/images/user.svg" alt="" />
+                  <p>NicholasWM</p>
+                </div>
+              </div>
+            </a>
+          </article>
+          <a href="" className={styles.buttonMorePosts}>
+            Carregar mais posts
+          </a>
+        </main>
+      </body>
+    </>
+
+  )
+}
 
 // export const getStaticProps = async () => {
 //   // const prismic = getPrismicClient();
